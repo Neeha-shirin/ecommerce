b26904b1220b05b5379f820decc5477bffbdfbad
Neeha-shirin <neehasherin987@gmail.com>
2025-01-08 11:48:31 +0530

    final

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -76,17 +76,17 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+from decouple import config
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'project',  
-        'USER': 'postgres',  
-        'PASSWORD': 'neeha123',  
-        'HOST': 'localhost',  
-        'PORT': '5432',  
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='project'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
-    
 }
 
 
